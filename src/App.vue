319293c3082533095fe81f1e7f6804a80a197f71
Utopia-in-reality <1836075887@qq.com>
2020-12-11 11:43:21 +0800

    blankvue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './views/Home.vue'
+export default {
+    name: 'App',
+  components: {
+    Home,
+    
+  }
+}
+</script>
+<style lang="less">
+#app {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    这是初始vuehhhh 
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+.home{
+        width: 100%;
+        height: 100%;
+}
+</style>
